d78ef8cd6a890c35c23103e56c22ca37762510e8
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-20 13:43:49 -0300

    functiom multiplicar por dois

---
+++ Exercício 37/script.js
@@ -0,0 +1,16 @@
+function dizerNome() {
+    console.log("João Pedro");
+}
+
+dizerNome();
+
+
+function multiplicarPorDois(valor) {
+    return valor * 2;
+}
+
+//console.log(multiplicarPorDois(10))
+
+let result = multiplicarPorDois(5)
+
+console.log(result);
