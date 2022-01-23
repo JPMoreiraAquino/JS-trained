6f6f03bc91d8705071a16ac88e0af5140224d1b7
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-22 21:28:25 -0300

    if else

---
+++ Exercício 42/script.js
@@ -0,0 +1,15 @@
+
+let valorMaior = eMaior(10, 10);
+
+console.log(valorMaior)
+
+function eMaior(n1, n2) {
+    return n1 > n2 ? n1: n2;
+}
+
+let n1 = 10
+let n2 = 3
+
+let ifelse1line = n1 > n2 ? n1 : n2;
+
+console.log(ifelse1line)
